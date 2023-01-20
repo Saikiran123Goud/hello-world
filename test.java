b6e3447bd6af76e8baf2ea8431b2cbfa50bbbd0c
Saikiran123Goud <122962964+Saikiran123Goud@users.noreply.github.com>
2023-01-20 11:16:23 +0530

    Add files via upload

---
+++ test.java
@@ -0,0 +1,17 @@
+package testing;
+
+public class test {
+	
+	@Test
+	public void pratice() {
+	
+		System.out.println("start");
+	}
+	 @Test
+	public void practice2() 
+	{
+		System.out.println("end");
+	}
+
+	}
+
